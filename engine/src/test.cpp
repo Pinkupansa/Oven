6d6c985c21b34969512a93f7904f90b1727a521a
Pinkupansa <sacha.cerf@polytechnique.org>
2026-05-24 18:40:55 +0200

    setup sandbox and dynamic linking

---
+++ engine/src/test.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "test.h"
+
+namespace Oven{ 
+    void Print(){
+        printf("Welcome to the Oven Game Engine! \n");
+    }
+}
---
+++ sandbox/src/application.cpp
@@ -0,0 +1,7 @@
+#include <iostream> 
+#include "config.h"
+#include "test.h"
+
+int main(){
+    Oven::Print();
+}
