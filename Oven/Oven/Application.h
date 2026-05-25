863dab9a0c049bdd788020ac5b3a84c449856cce
Pinkupansa <sacha.cerf@polytechnique.org>
2026-05-25 18:45:14 +0200

    added window

--- Oven/Oven/Application.h
+++ Oven/Oven/Application.h
@@ -1,12 +1,15 @@
 #pragma once 
 #include "Oven/Core.h"
+#include "Oven/Window.h"
 namespace Oven{
     class OVEN_API Application{
         public:
             Application();
             virtual ~Application();
             void Run();
-
+        private: 
+            std::unique_ptr<Window> m_Window; 
+            bool m_Running = true;
     };
 
     //define in client
